fa404ec343981715f1067bc7e8007bcfb2b2b7de
chihwayi <chihwayii@outlook.com>
2024-06-27 11:30:27 +0200

    cors filter added

---
+++ src/main/java/zw/org/mohcc/hiv_dashboard/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package zw.org.mohcc.hiv_dashboard.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                //.allowedOrigins("http://173.212.195.88:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
+                .allowedHeaders("*");
+    }
+}
